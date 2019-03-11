d4f775150f9a4f5a950bb917ad23b26e0dbb9010
Odysseus <arwaterman@gmail.com>
2019-03-11 15:20:26 -0700

    adding script logic to watch how many people are connected to the application

---
+++ assets/javascript/app.js
@@ -0,0 +1,70 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDkzgePzwnfeMNxusbK89JKnuarexpub_M",
+    authDomain: "rps-multiplayer-74bcc.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-74bcc.firebaseio.com",
+    projectId: "rps-multiplayer-74bcc",
+    storageBucket: "",
+    messagingSenderId: "1028361656334"
+  };
+
+firebase.initializeApp(config);
+
+let database = firebase.database();
+
+// * Create a game that suits this user story:
+
+// * Only two users can play at the same time.
+
+// * Both players pick either `rock`, `paper` or `scissors`. After the players make their selection, the game will tell them whether a tie occurred or if one player defeated the other.
+
+// * The game will track each player's wins and losses.
+
+// * Throw some chat functionality in there! No online multiplayer game is complete without having to endure endless taunts and insults from your jerk opponent.
+
+// * Styling and theme are completely up to you. Get Creative!
+
+// * Deploy your assignment to Github Pages.
+
+// -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snapshot) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+
+    let num = snapshot.numChildren();
+
+    if (num === 1) {
+        document.getElementById("watchers").textContent = num + " person is watching";
+    }
+    else {
+        document.getElementById("watchers").textContent = num + " people are watching";
+    }
+    
+});
+
+// -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
