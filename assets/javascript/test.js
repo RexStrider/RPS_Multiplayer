840e69b0a65ee93ceb21b573b9311a3a58262e87
Odysseus <arwaterman@gmail.com>
2019-03-16 22:33:44 -0700

    game compares players choices

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -164,6 +164,57 @@ let player = {
         // check database for other players
         database.ref("janken").on("child_added", data => {
             console.log(data.val());
+
+            let opponent = data.val();
+
+            if (opponent.name != player.name && data.hasChild("choice")) {
+                if ((player.choice == "rock" && opponent.chioce == "scissors")
+                || (player.choice == "scissors" && opponent.choice == "paper")
+                || (player.choice == "paper" && opponent.choice == "rock")) {
+                    console.log ("you win " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You win!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+                else if ((player.choice == "rock" && opponent.chioce == "paper")
+                || (player.choice == "scissors" && opponent.choice == "rock")
+                || (player.choice == "paper" && opponent.choice == "scissors")) {
+                    console.log ("you lose " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You lose!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+                else {
+                    console.log ("you tied " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You tied!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+            }
         });
     }
 }
