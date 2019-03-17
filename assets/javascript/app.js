00486b29191dbe1041acc3d6030363d5679c064a
Odysseus <arwaterman@gmail.com>
2019-03-17 15:54:41 -0700

    updated message prompts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,7 +51,7 @@ let player = {
                 falseName = true;
             }
             else {
-                document.getElementById("user-info").innerHTML = "<p>Awesome, your name is '<span class='bold'>" + player.name + "</span>'.</p>";
+                document.getElementById("user-info").innerHTML = "<p>Welcome <span class='bold'>" + player.name + "</span>, to the most dangerous game.</p>";
                 database.ref("player").push({
                     name: player.name,
                 }).onDisconnect().remove();
@@ -81,7 +81,7 @@ let player = {
                     }
                     else {
                         document.getElementById("submit").removeEventListener("click", submitEvent2);
-                        document.getElementById("user-info").innerHTML = "<p>Ok, your name is '<span class='bold'>" + player.name + "</span>'.</p>";
+                        document.getElementById("user-info").innerHTML = "<p>Welcome <span class='bold'>" + player.name + "</span>, to the most dangerous game.</p>";
                         database.ref("player").push({
                             name: player.name,
                         }).onDisconnect().remove();
@@ -96,7 +96,7 @@ let player = {
         let count = 5;
         let intervalId = setInterval(() => {
             if (count > 0) {
-                document.getElementById("rps").innerHTML = "<p>"+count+" seconds left</p>";
+                document.getElementById("rps").innerHTML = "<p>Game starts in "+count+" seconds</p>";
                 count--;
             }
             else {
@@ -108,7 +108,7 @@ let player = {
     },
 
     startGame: () => {
-        console.log("Game start!");
+        // console.log("Game start!");
 
         document.getElementById("user-info").innerHTML = "<p>Choose your weapon...</p>";
 
@@ -156,12 +156,12 @@ let player = {
         database.ref("janken/" + player.name).onDisconnect().remove();
 
         // database.ref("janken/" + player.name).onDisconnect().remove();
-        document.getElementById("user-info").innerHTML = "<p>One moment please...</p>";
+        document.getElementById("user-info").innerHTML = "<p>Please waiting for a new challenger...</p>";
         document.getElementById("rps").innerHTML = "";
 
         // check database for other players
         database.ref("janken").on("child_added", data => {
-            console.log(data.val());
+            // console.log(data.val());
 
             let opponent = data.val();
 
@@ -169,7 +169,7 @@ let player = {
                 if ((player.choice == "rock" && opponent.choice == "scissors")
                 || (player.choice == "scissors" && opponent.choice == "paper")
                 || (player.choice == "paper" && opponent.choice == "rock")) {
-                    console.log ("you win " + player.name);
+                    // console.log ("you win " + player.name);
 
                     let p1 = document.createElement("p");
                     let p2 = document.createElement("p");
@@ -177,7 +177,7 @@ let player = {
 
                     p1.textContent = "You win!";
                     p2.textContent = "You chose " + player.choice;
-                    p3.textContent = "Your opponent chose " + opponent.choice;
+                    p3.textContent = opponent.name + " chose " + opponent.choice;
 
                     document.getElementById("user-info").innerHTML = "";
                     document.getElementById("rps").append(p1, p2, p3);
@@ -185,7 +185,7 @@ let player = {
                 else if ((player.choice == "rock" && opponent.choice == "paper")
                 || (player.choice == "scissors" && opponent.choice == "rock")
                 || (player.choice == "paper" && opponent.choice == "scissors")) {
-                    console.log ("you lose " + player.name);
+                    // console.log ("you lose " + player.name);
 
                     let p1 = document.createElement("p");
                     let p2 = document.createElement("p");
@@ -193,13 +193,13 @@ let player = {
 
                     p1.textContent = "You lose!";
                     p2.textContent = "You chose " + player.choice;
-                    p3.textContent = "Your opponent chose " + opponent.choice;
+                    p3.textContent = opponent.name + " chose " + opponent.choice;
 
                     document.getElementById("user-info").innerHTML = "";
                     document.getElementById("rps").append(p1, p2, p3);
                 }
                 else {
-                    console.log ("you tied " + player.name);
+                    // console.log ("you tied " + player.name);
 
                     let p1 = document.createElement("p");
                     let p2 = document.createElement("p");
@@ -207,7 +207,7 @@ let player = {
 
                     p1.textContent = "You tied!";
                     p2.textContent = "You chose " + player.choice;
-                    p3.textContent = "Your opponent chose " + opponent.choice;
+                    p3.textContent = opponent.name + " chose " + opponent.choice;
 
                     document.getElementById("user-info").innerHTML = "";
                     document.getElementById("rps").append(p1, p2, p3);
