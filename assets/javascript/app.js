6af545c873b0036608a86ec83c64b669151fcfdf
Odysseus <arwaterman@gmail.com>
2019-03-17 15:30:19 -0700

    updated app logic script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDkzgePzwnfeMNxusbK89JKnuarexpub_M",
     authDomain: "rps-multiplayer-74bcc.firebaseapp.com",
@@ -12,193 +11,214 @@ firebase.initializeApp(config);
 
 let database = firebase.database();
 
-// * Create a game that suits this user story:
+let player = {
+    name: "",
+    choice: "",
 
-// * Only two users can play at the same time.
+    intervalId: null,
 
-// * Both choiceRefs pick either `rock`, `paper` or `scissors`. After the choiceRefs make their selection, the game will tell them whether a tie occurred or if one choiceRef defeated the other.
+    getPlayerName: () => {
+        document.getElementById("user-info").innerHTML = "";
 
-// * The game will track each choiceRef's wins and losses.
+        let form = document.createElement("form");
 
-// * Throw some chat functionality in there! No online multichoiceRef game is complete without having to endure endless taunts and insults from your jerk opponent.
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        let button = document.createElement("button");
 
-// * Styling and theme are completely up to you. Get Creative!
+        label.setAttribute("id", "user-prompt");
+        input.setAttribute("id", "input-name");
+        button.setAttribute("id", "submit");
 
-// * Deploy your assignment to Github Pages.
+        label.innerText = "Please enter your name ";
+        button.innerText = "Submit";
 
-// -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-// connectionsRef references a specific location in our database.
-// All of our connections will be stored in this directory.
-let connectionsRef = database.ref("/connections");
+        form.append(label, input, button);
 
-// '.info/connected' is a special location provided by Firebase that is updated every time
-// the client's connection state changes.
-// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-let connectedRef = database.ref(".info/connected");
+        document.getElementById("user-info").append(form);
 
-// When the client's connection state changes...
-connectedRef.on("value", snap => {
+        document.getElementById("submit").addEventListener("click", function submitEvent(event) {
+            event.preventDefault();
 
-  // If they are connected..
-  if (snap.val()) {
+            document.getElementById("submit").removeEventListener("click", submitEvent);
+            
+            player.name = document.getElementById("input-name").value;
 
-    // Add user to the connections list.
-    let con = connectionsRef.push(true);
+            let falseName = false;
 
-    // Remove user from the connection list when they disconnect.
-    con.onDisconnect().remove();
-  }
-});
-
-// When first loaded or when the connections list changes...
-let firebaseConnectionEstablished = new Promise( (resolve, reject) => {
-    connectionsRef.on("value", snapshot => {
-
-        // Display the viewer count in the html.
-        // The number of online users is the number of children in the connections list.
-      
-          let num = snapshot.numChildren();
-      
-          if (num === 1) {
-              document.getElementById("watchers").textContent = num + " person is watching";
-          }
-          else {
-              document.getElementById("watchers").textContent = num + " people are watching";
-          }
-          
-          resolve(num);
-      });
-}).catch( error => {
-    console.log("error ocurred during the promise, firebase connection established")
-    console.log(error);
-});
-
-// -------------------------------------------------------------- (CRITICAL - BLOCK) --------------------------- //
-let name;
-
-let nameRef;
-let choiceRef;
-
-let duelers = [];
-
-firebaseConnectionEstablished.then( () => {
-
-    name = prompt("What's your name?");
-    while (name === "" || name === null) {
-        name = prompt("Give me a proper name!");
-    }
-
-    let playerIsSet = new Promise((resolve, reject) => {
-        setPlayer();
-        // console.log("during FCE: " + name);
-        resolve(name);
-    });
-    
-    playerIsSet.then( () => {
-        let listElements = document.getElementsByTagName("li");
-
-        for (i=0; i < listElements.length; i++) {
-
-            let element = listElements.item(i);
-            element.addEventListener("click", () => {
-
-                let choice = element.getAttribute("data-value");
-
-                // console.log(choice);
-
-                // save choice to database
-                choiceRef.set({
-                    name: name,
-                    choice: choice
-                }).then(() => {
-
-                    let userChoices = database.ref("users/choice");
-                    
-                    userChoices.on("child_added", function(data) {
-
-                        console.log(data.val());
-
-                        duelers.push(data.val());
-
-                        console.log(duelers);
-
-                        if (duelers.length > 1) {
-                            console.log("it's go time!");
-                            
-                            if (duelers[0].name === name) {
-                                determineWinner(duelers[0].choice, duelers[1].choice);
-                            }
-                            else if(duelers[1].name === name) {
-                                determineWinner(duelers[1].choice, duelers[0].choice);
-                            }
-                            else {
-                                console.log("sorry "+name+", better wait your turn...");
-                            }
-
-                        }
-                     })
-                })
-            });
-        }
-    }).catch( error => {
-        console.log("an error ocurred during the promise, the player is set");
-        console.log(error);
-    });
-    
-});
-
-
-
-function setPlayer() {
-    nameRef = database.ref("users/"+name)
-    choiceRef = database.ref("users/choice/"+name);
-
-    nameRef.once("value", snapshot => {
-        if (snapshot.exists()) {
-            name = prompt("That name already exists, enter a new name");
-            while (name === "" || name === null) {
-                name = prompt("If you want to play the game, then you must enter a name");
+            if (player.name === "" || player.name == null) {
+                document.getElementById("user-prompt").textContent = "That's not a name. Please enter a name ";
+                falseName = true;
             }
-            setPlayer(name);
-        }
-        else {
+            else {
+                document.getElementById("user-info").innerHTML = "<p>Awesome, your name is '<span class='bold'>" + player.name + "</span>'.</p>";
+                database.ref("janken/" + player.name).set({
+                    name: player.name,
+                });
+                database.ref("janken/" + player.name).onDisconnect().remove();
+                player.countDown();
+            }
 
-            nameRef.set({
-                name: name
-            });
+            if (falseName) {
+                let count = 0;
+                document.getElementById("submit").addEventListener("click", function submitEvent2(event) {
+                    event.preventDefault();
 
-            nameRef.onDisconnect().remove();
-            choiceRef.onDisconnect().remove();
-        }
-    
-    }).catch(error => {
-        console.log("Uh oh... there has been an error");
-        console.log(error);
-    });
+                    player.name = document.getElementById("input-name").value;
+
+                    let tries = 5;
+                    if ((player.name === "" || player.name == null) && count < tries) {
+                        document.getElementById("user-prompt").textContent = "That's not a name, you have " + (tries - count) + " more tries to pick a name ";
+                        count++;
+                    }
+                    else if ((player.name === "" || player.name == null) && count >= tries) {
+                        document.getElementById("submit").removeEventListener("click", submitEvent2);
+                        player.name = Math.random().toString(36).replace('0.', '').substr(0, 8);
+                        document.getElementById("user-info").innerHTML = "<p>Fine, I'll pick a name for you... Your new name is '<span class='bold'>" + player.name + "</span>'. You're welcome...</p>";
+                        database.ref("janken/" + player.name).set({
+                            name: player.name,
+                        });
+                        database.ref("janken/" + player.name).onDisconnect().remove();
+                        player.countDown();
+                    }
+                    else {
+                        document.getElementById("submit").removeEventListener("click", submitEvent2);
+                        document.getElementById("user-info").innerHTML = "<p>Ok, your name is '<span class='bold'>" + player.name + "</span>'.</p>";
+                        database.ref("janken/" + player.name).set({
+                            name: player.name,
+                        });
+                        database.ref("janken/" + player.name).onDisconnect().remove();
+                        player.countDown();
+                    }
+                });
+            }
+        });
+    },
+
+    countDown: () => {
+        let count = 5;
+        let intervalId = setInterval(() => {
+            if (count > 0) {
+                document.getElementById("rps").innerHTML = "<p>"+count+" seconds left</p>";
+                count--;
+            }
+            else {
+                document.getElementById("rps").innerHTML = "";
+                clearInterval(intervalId);
+                player.startGame();
+            }
+        }, 1000);
+    },
+
+    startGame: () => {
+        console.log("Game start!");
+
+        document.getElementById("user-info").innerHTML = "<p>Choose your weapon...</p>";
+
+        let ul = document.createElement("ul");
+
+        let rock = document.createElement("li");
+        let paper = document.createElement("li");
+        let scissors = document.createElement("li");
+
+        rock.setAttribute("id", "rock");
+        paper.setAttribute("id", "paper");
+        scissors.setAttribute("id", "scissors");
+
+        rock.textContent = "Give them the rock!";
+        paper.textContent = "Serve them those papers!";
+        scissors.textContent = "Run with those scissors!";
+
+        ul.append(rock, paper, scissors);
+
+        document.getElementById("rps").append(ul);
+
+        rock.addEventListener("click", () => {
+            player.saishoWaGuujankenPo("rock");
+        });
+
+        paper.addEventListener("click", () => {
+            player.saishoWaGuujankenPo("paper");
+        });
+
+        scissors.addEventListener("click", () => {
+            player.saishoWaGuujankenPo("scissors");
+        });
+    },
+
+    // This function handles the storing of the move thrown, both locally and in the firebase backend
+    // It is named after the Japanese ritual performed before playing rock, paper, scissors
+    // If you still don't understand the name, look into the Hunter x Hunter anime or wikipedia...
+    saishoWaGuujankenPo: throwMove => {
+        player.choice = throwMove;
+
+        database.ref("janken/" + player.name).set({
+            name: player.name,
+            choice: throwMove
+        });
+
+        // database.ref("janken/" + player.name).onDisconnect().remove();
+        document.getElementById("user-info").innerHTML = "<p>One moment please...</p>";
+        document.getElementById("rps").innerHTML = "";
+
+        // check database for other players
+        database.ref("janken").on("child_added", data => {
+            console.log(data.val());
+
+            let opponent = data.val();
+
+            if (opponent.name != player.name && data.hasChild("choice")) {
+                if ((player.choice == "rock" && opponent.chioce == "scissors")
+                || (player.choice == "scissors" && opponent.choice == "paper")
+                || (player.choice == "paper" && opponent.choice == "rock")) {
+                    console.log ("you win " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You win!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+                else if ((player.choice == "rock" && opponent.chioce == "paper")
+                || (player.choice == "scissors" && opponent.choice == "rock")
+                || (player.choice == "paper" && opponent.choice == "scissors")) {
+                    console.log ("you lose " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You lose!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+                else {
+                    console.log ("you tied " + player.name);
+
+                    let p1 = document.createElement("p");
+                    let p2 = document.createElement("p");
+                    let p3 = document.createElement("p");
+
+                    p1.textContent = "You tied!";
+                    p2.textContent = "You chose " + player.choice;
+                    p3.textContent = "Your opponent chose " + opponent.choice;
+
+                    document.getElementById("user-info").innerHTML = "";
+                    document.getElementById("rps").append(p1, p2, p3);
+                }
+            }
+        });
+    }
 }
 
-function determineWinner(you, them) {
-    if ( (you === 'r' && them === 's')
-      || (you === 's' && them === 'p')
-      || (you === 'p' && them === 'r')) {
-        console.log("You win!");
-        // alert("You win! Congratulations " + name);
+player.getPlayerName();
 
-        document.getElementById("rps").innerHTML = "<p>Congratulations " + name + ", you won!</p>";
-    }
-    else if ( (you === 'r' && them === 'p')
-           || (you === 's' && them === 'r')
-           || (you === 'p' && them === 's')) {
-        console.log("You lose!");
-        // alert("You lose! Sorry " + name);
-        document.getElementById("rps").innerHTML = "<p>Boo " + name + ", you stink!</p>";
-    }
-    else {
-        console.log("You tied...");
-        // alert("You tied... better luck next time");
-        document.getElementById("rps").innerHTML = "<p>You tied? how anti-climatic... better luck nex time!</p>";
-    }
-
-    console.log("goodbye "+duelers.pop().name + "!");
-    console.log("farewell "+duelers.pop().name + "!");
-}
+// after getting player name, then we must start the game
